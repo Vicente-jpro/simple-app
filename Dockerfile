210b825613772b606fe8d849cfcf7f72fa58aff0
vicente-jpro <vicenteviciii@outlook.com>
2026-02-07 17:17:15 +0100

    feat: env variables added.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
 FROM eclipse-temurin:17
-COPY --from=public.ecr.aws/awsguru/aws-lambda-adapter:0.8.4 /lambda-adapter /opt/extensions/lambda-adapter
+# Lambda dapter.
+#COPY --from=public.ecr.aws/awsguru/aws-lambda-adapter:0.8.4 /lambda-adapter /opt/extensions/lambda-adapter
 VOLUME /tmp
 EXPOSE 8080 
 ADD  ./target/simple-app-0.0.1-SNAPSHOT.jar  simple-app.jar
 ENTRYPOINT ["java", "-jar", "/simple-app.jar"]
 
 # docker build -t vicentesimao/simple-app:latest .
+
+# Use this command to push the image to Docker Hub: using your stack.
+#aws ecr-public get-login-password --region us-east-1  docker> | docker login --username AWS --password-stdin public.ecr.aws
 
--- src/main/java/com/example/simple_app/HelloController.java
+++ src/main/java/com/example/simple_app/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.simple_app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HelloController {
     
+    @Value("${environment.variable1}")
+    private String envVar1;
+
+    @Value("${environment.variable2}")
+    private String envVar2;
+
     @GetMapping
     public String hello() {
-        return "Hello from simple_app!";
+        return "Hello from simple_app!" +envVar1 + " " + envVar2;
     }
 }
